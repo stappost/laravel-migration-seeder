f0259ac4265a94f784b11ddde1b8625a9acffd7e
stappost <emanuele.scarpantoni@gmail.com>
2024-02-19 08:27:01 +0100

    create table in db

---
+++ database/migrations/2024_02_19_072240_create_trains_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 50);
+            $table->string('stazione_partenza', 50);
+            $table->string('stazione_arrivo', 50);
+            $table->dataTime('orario_partenza', 50);
+            $table->dataTime('orario_arrivo', 50);
+            $table->string('codice_treno', 7);
+            $table->boolean('in_orario', 50);
+            $table->boolean('cancellato', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
